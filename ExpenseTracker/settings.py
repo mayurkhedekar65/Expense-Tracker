43ebce91063fae8b5faf779415f417340c01fbd5
mayurkhedekar65 <mayurkhedekar646@gmail.com>
2025-08-03 14:12:05 +0530

    Frontend Created

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'ExpenseTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'expense_tracker',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',}}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -115,7 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+import os
+STATIC_ROOT = 'mystaticfiles'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'mystaticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'public/static')]
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
